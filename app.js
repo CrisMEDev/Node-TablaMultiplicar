c45061eb4e5ea0cb85148c3acf5c7881e1c2986b
Cristian Morales <cr15moresp@gmail.com>
2021-05-22 01:30:39 -0500

    Se instaló el paquete yargs

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const { crearArchivo } = require('./helpers/multiplicar') 
-
+const { crearArchivo } = require('./helpers/multiplicar'); 
+const argv = require('yargs').argv; // Se extrae la propiedad argv del paquete yargs
 
 console.clear();
 
-
+// --------------------------------------------------------------------------------------
 // console.log(process.argv);  // Imprime los argumentos enviados desde consola
-const [ , , arg3 = '--base=5' ] = process.argv;     // Se asigna un valor por defecto a arg3 para evitar el undefined
-const [ , base = '5' ] = arg3.split('=');
-console.log(arg3);
-console.log(base);
+// const [ , , arg3 = '--base=5' ] = process.argv;     // Se asigna un valor por defecto a arg3 para evitar el undefined
+// const [ , base = '5' ] = arg3.split('=');
+// console.log(arg3);
+// console.log(base);
+// --------------------------------------------------------------------------------------
 
+// En consola colorcar: node app --base=5 ó node app --base 5
+// node app --help  // Muestra las opciones para la app que se está desarrollando
+console.log( argv );
+console.log( 'yargs base: ', argv.base );
 
 
 
@@ -17,8 +22,8 @@ console.log(base);
 
 // Tabla de multiplicar 
 // const base = 3;
-crearArchivo( base )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch(err => console.log(err));
+// crearArchivo( base )
+//     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+//     .catch(err => console.log(err));
 
 
