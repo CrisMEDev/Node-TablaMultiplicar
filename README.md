f83d1c3b497e048da9871a780e639c14beb6f940
Cristian Morales <cr15moresp@gmail.com>
2021-05-21 19:58:03 -0500

    Se creó la tabla del 5

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bases de node
+
+Explicando las bases de node con este proyecto
+
---
+++ app.js
@@ -0,0 +1,11 @@
+
+console.clear();
+
+console.log('==========================');
+console.log('Tabla del 5');
+console.log('==========================');
+
+// Tabla del 5
+for( let i = 1; i <= 10; i++ )
+    console.log( `5 * ${i} = ${5 * i}` );
+
