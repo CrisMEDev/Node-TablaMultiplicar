ad35cbeedacbb48388780fb1728016a0c11616f2
Cristian Morales <cr15moresp@gmail.com>
2021-05-22 00:32:59 -0500

    Se recibió información desde la linea de comandos con process.argv

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear();
 
+
+// console.log(process.argv);  // Imprime los argumentos enviados desde consola
+const [ , , arg3 = '--base=5' ] = process.argv;     // Se asigna un valor por defecto a arg3 para evitar el undefined
+const [ , base = '5' ] = arg3.split('=');
+console.log(arg3);
+console.log(base);
+
+
+
+
+
+
 // Tabla de multiplicar
-const base = 3;
+// const base = 3;
 crearArchivo( base )
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
     .catch(err => console.log(err));
