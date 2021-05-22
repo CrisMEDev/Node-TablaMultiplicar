7f7d73e27a48da710bbad2c43a9d1b5faf7d7a37
Cristian Morales <cr15moresp@gmail.com>
2021-05-22 09:41:20 -0500

    se cambio yargs config a un archivo independiente

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const { crearArchivo } = require('./helpers/multiplicar'); 
-const argv = require('yargs')
-        .option( 'b', {
-            alias: 'base',
-            type:'number',
-            demandOption: true,
-            description: 'La base para la tabla de multiplicar'
-        })
-        .option( 'l', {
-            alias: 'listar',
-            type:'boolean',
-            default: false,
-            description: 'Decide si mostrar o no la tabla en consola'
-        })
-        .check( (argv, options) => {
-            if ( isNaN( argv.b ) ) throw 'La base tiene que ser un número';
-            return true;  // Si no hay error devuelve true
-        })
-        .argv; // Se extrae la propiedad argv del paquete yargs
-
+const argv = require('./config/yargs');
 
 console.clear();
 
