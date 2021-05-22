afc5efbe49a9b90fa9b6781d0582352f9762255f
Cristian Morales <cr15moresp@gmail.com>
2021-05-21 20:16:15 -0500

    Se agregó la librería de file system y se uso para generar un archivo de tabla de multiplicar

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')    // Se carga la libreria para file system
+
 
 console.clear();
 
@@ -5,7 +7,17 @@ console.log('==========================');
 console.log('Tabla del 5');
 console.log('==========================');
 
-// Tabla del 5
+// Tabla de multiplicar
+let salida = '';
+const base = 3;
 for( let i = 1; i <= 10; i++ )
-    console.log( `5 * ${i} = ${5 * i}` );
+    salida += `${base} * ${i} = ${base * i}\n` ;
+
+console.log(salida);
+
+
+fs.writeFile( `tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log('Archivo creado');
+});
 
